074991ae9c2afc8dc72bedd45fbd3fddf3de6ea0
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2021-02-13 21:51:21 +0900

    画面遷移完了

--- src/components/PageA.tsx
+++ src/components/PageA.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-const PageA = () => {
+const PageA: React.FC<{}> = () => {
+
     return (
         <div>
-            <h1>PageB</h1>
+            <h1>PageA</h1>
+            <Link to="PageB">PageB</Link> /
+            <Link to="Home">Homeへ戻る</Link>
         </div>
     );
 };
--- src/components/PageB.tsx
+++ src/components/PageB.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-const PageB = () => {
+const PageB: React.FC<{}> = () => {
+
     return (
         <div>
             <h1>PageB</h1>
+            <Link to="pageA">PageA</Link> /
+            <Link to="Home">Homeへ戻る</Link>
         </div>
     );
 };
