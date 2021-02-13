fc14b3278ca79f88e2d2b46a379db2042c6ed9ed
yo-yan <yooyan0512@gmail.com>
2021-02-13 16:44:07 +0900

    PageaB おみくじ

--- src/components/PageB.tsx
+++ src/components/PageB.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 
 const PageB = () => {
+
+    const [omikuji, setOmikuji] = useState<string>('')
+
+    const omikujihand = () => {
+        const num = Math.floor(Math.random() * 6)
+        if (num === 0) {
+            setOmikuji('大吉')
+        } else if (num === 1) {
+            setOmikuji('末吉')
+        } else if (num === 2) {
+            setOmikuji('中吉')
+        } else if (num === 3) {
+            setOmikuji('小吉')
+        } else if (num === 4) {
+            setOmikuji('吉')
+        } else if (num === 5) {
+            setOmikuji('凶')
+        } else {
+            setOmikuji('大凶')
+        }
+    }
+
+
     return (
         <div>
-            <h1>PageB</h1>
+            <h1>今日のおみくじ</h1>
+
+            <button onClick={omikujihand}>占う</button>
+            {omikuji}
         </div>
     );
 };
