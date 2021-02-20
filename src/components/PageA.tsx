abe4aca21e3063e7bae5f91882e7e4794bb729a1
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-02-20 11:23:06 +0900

    code review実施しました！

--- src/components/PageA.tsx
+++ src/components/PageA.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
+/**
+ * PageA画面
+ * API取得
+ * 感想
+ * > この画面はアプリのViewを作成する上で、最も大切なJavaScriptの操作方法を学んでいただいております。
+ *   難易度はかなり高めですが、がんばってください！
+ * @author hatuki
+ */
 const PageA: React.FC<{}> = () => {
-
-    return (
-        <div>
-            <h1>PageA</h1>
-            <Link to="PageB">PageB</Link> /
-            <Link to="Home">Homeへ戻る</Link>
-        </div>
-    );
+  return (
+    <div>
+      <h1>PageA</h1>
+      <Link to="PageB">PageB</Link> /<Link to="Home">Homeへ戻る</Link>
+    </div>
+  );
 };
 
 export default PageA;
