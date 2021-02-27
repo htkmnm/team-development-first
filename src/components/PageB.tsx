2b53e4dbbbb1af32c4b595c475f9092b6de19993
yo-yan <yooyan0512@gmail.com>
2021-02-27 21:53:32 +0900

    topic-0009

--- src/components/PageB.tsx
+++ src/components/PageB.tsx
@@ -9,34 +9,34 @@ import { useState } from 'react';
  *   あとはPageBのところにPageB: React.FC<{}>とすると、後々の実装が楽になります！
  * @author hatuki
  */
-const PageB = () => {
-  const [omikuji, setOmikuji] = useState<string>('');
+const PageB: React.FC<{}> = () => {
+    const [omikuji, setOmikuji] = useState<string>('');
 
-  const omikujihand = () => {
-    const num = Math.floor(Math.random() * 6);
-    if (num === 0) {
-      setOmikuji('大吉');
-    } else if (num === 1) {
-      setOmikuji('末吉');
-    } else if (num === 2) {
-      setOmikuji('中吉');
-    } else if (num === 3) {
-      setOmikuji('小吉');
-    } else if (num === 4) {
-      setOmikuji('吉');
-    } else if (num === 5) {
-      setOmikuji('凶');
-    } else {
-      setOmikuji('大凶');
-    }
-  };
-  return (
-    <div>
-      <h1>今日のおみくじ</h1>
-      <button onClick={omikujihand}>今日の占い</button>
-      {omikuji}
-    </div>
-  );
+    const omikujihand = () => {
+        const num = Math.floor(Math.random() * 6);
+        if (num === 0) {
+            setOmikuji('大吉');
+        } else if (num === 1) {
+            setOmikuji('末吉');
+        } else if (num === 2) {
+            setOmikuji('中吉');
+        } else if (num === 3) {
+            setOmikuji('小吉');
+        } else if (num === 4) {
+            setOmikuji('吉');
+        } else if (num === 5) {
+            setOmikuji('凶');
+        } else {
+            setOmikuji('大凶');
+        }
+    };
+    return (
+        <div>
+            <h1>今日のおみくじ</h1>
+            <button onClick={omikujihand}>今日の占い</button>
+            {omikuji}
+        </div>
+    );
 };
 
 export default PageB;
