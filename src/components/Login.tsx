81cad15b0705877c60592004bbdc3358bbde06d3
yo-yan <yooyan0512@gmail.com>
2021-03-13 21:16:54 +0900

    firebase接続完了

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
-import firebase from '../config/firebase'
 import { TextField } from '@material-ui/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header"
+import { userPlus } from '../config/firebase'
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleClick = () => {
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((Users) => {
-                console.log(Users)
-            })
+    const handleClick = async () => {
+        await userPlus()
+        console.log('fin')
     }
     return (
         <div>
